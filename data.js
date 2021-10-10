f6ac226e5180fba65a8b398ebcfce49dd8bcc453
GitBulll <88339413+GitBulll@users.noreply.github.com>
2021-10-10 16:25:55 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,46 @@
+const data = [
+    {
+      id: 1,
+      email: 'berlin.green@gmail.in',
+      first_name: 'Berlin',
+      last_name: 'Green',
+      avatar: 'https://cdna.artstation.com/p/assets/images/images/029/075/126/large/soumik-choudhury-berlin.jpg?1596388689',
+    },
+    {
+      id: 2,
+      email: 'tokyo.gaz@gmail.in',
+      first_name: 'Tokyo',
+      last_name: 'Gazhdamb',
+      avatar: 'https://cdnb.artstation.com/p/assets/images/images/029/075/255/large/soumik-choudhury-tokio.jpg?1596389167',
+    },
+    {
+      id: 3,
+      email: 'lisbon.tribb@gmail.in',
+      first_name: 'Lisbon',
+      last_name: 'Tribbiani',
+      avatar: 'https://cdnb.artstation.com/p/assets/images/images/029/075/167/large/soumik-choudhury-lisbon.jpg?1596388899',
+    },
+    {
+      id: 4,
+      email: 'nairobi.geller@gmail.in',
+      first_name: 'Nairobi',
+      last_name: 'Geller',
+      avatar: 'https://cdna.artstation.com/p/assets/images/images/029/075/054/large/soumik-choudhury-nairobi.jpg?1596388438',
+    },
+    {
+      id: 5,
+      email: 'professor.perry@gmail.in',
+      first_name: 'Professor',
+      last_name: 'Perry',
+      avatar: 'https://cdna.artstation.com/p/assets/images/images/030/681/744/large/mohammx-heshmati-b3.jpg?1601334751',
+    },
+    {
+      id: 6,
+      email: 'bogota.burke@gmail.in',
+      
+      first_name: 'Bogota',
+      last_name: 'Burke',
+      avatar: 'https://i.etsystatic.com/18097100/r/il/a91419/3352702069/il_1588xN.3352702069_mcwy.jpg',
+    },
+  ];
+  export default data;
